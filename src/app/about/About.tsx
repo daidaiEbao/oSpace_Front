fd157c19b13a3db69f0a07d9d103eab38abe2900
daidaiEbao <ailife965@gmail.com>
2024-03-22 12:19:27 +0800

    feat: basic frame

---
+++ src/app/about/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+const About : React.FC = () => {
+    return (
+        <div>About
+            <Button>Default Button</Button>
+        </div>
+    );
+}
+
+export default About; 
